f689ed7b1f20f6bd01ee5c74c699abfdacd43073
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-02-02 23:21:11 -0300

    tela

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
 import os
-import random
-import neat
 
-ai_jogando = True
+ai_jogando = False
 geracao = 0
 
 TELA_LARGURA = 500
@@ -25,6 +23,7 @@ class Tower:
     VELOCIDADE_ROTACAO = 20
     TEMPO = 5
 
+
     def __init__(self, attack, defense, shooting_speed, money, coins):
         self.attack = attack
         self.defense = defense
@@ -32,3 +31,35 @@ class Tower:
         self.money = money
         self.coins = coins
 
+
+    def shoot(self):
+        self.shooting_speed = self.shooting_speed
+        self.attack = self.attack
+        self.fire = 50
+
+
+
+    def desenhar_tela(tela):
+        tela.blit(0, 0)
+
+        for torre in torres:
+            torre.desenhar(tela)
+
+        pygame.display.update()
+
+
+
+"""
+
+        texto = FONTE_PONTOS.render(f"Pontuação: {pontos}", 1, (255, 255, 255))
+        tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
+
+
+"""
+"""
+
+    if ai_jogando:
+        texto = FONTE_PONTOS.render(f"Geração: {geracao}", 1, (255, 255, 255))
+        tela.blit(texto, (10, 10))
+
+"""
